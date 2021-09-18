771a1ba2105b8b18595ce36a4afc0eaf904966cd
Swapnil Kole <swapnilkole7500@gmail.com>
2021-09-18 11:59:25 +0530

    SPA added but images removed

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Menu from '../Components/MenuComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import DishDetail from './DishdetailComponent';
 import Footer from './FooterComponent';
 import Header from './HeaderComponent';
@@ -13,17 +16,30 @@ class MainComponent extends Component{
         super(props);
         this.state = {
         dishes: DISHES,
+        comments: COMMENTS,
+        leaders: LEADERS,
+        promotions: PROMOTIONS,
         selectedDish: null
         };
     }
 
     render() {
-        console.log(this.state.dishes);
+        
+        const HomePage = () => {
+            return(
+                <Home 
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                    promotion={this.state.leaders.filter((promo) => promo.featured)[0]}
+                />
+            )
+        }
+
         return (
         <div>
             <Header/>
             <Switch>
-                <Route path="/home" component={Home} />
+                <Route path="/home" component={HomePage} />
                 <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
                 <Route exact path="/contactus" component={Contact}/>
                 <Redirect to="/home" />
