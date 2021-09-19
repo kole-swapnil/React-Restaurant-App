43995171b9370bcc46bfc408e54b48b1a9cd6fbf
Swapnil Kole <swapnilkole7500@gmail.com>
2021-09-19 22:59:19 +0530

    breadcrumb and SPA 2

--- src/Components/DishdetailComponent.js
+++ src/Components/DishdetailComponent.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom' 
 const DishDetail = (props) => {
-    if (props.selectedDish != null) {
-        let comments = props.selectedDish.comments.map((comment) => {
+    if (props.dish != null) {
+        let comments = props.comments.map((comment) => {
             return (
                 <>
                     <p>{comment.comment}</p>
@@ -14,12 +14,22 @@ const DishDetail = (props) => {
         return (
             <div className="container">
                 <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
                     <div className="col-12 col-md-5 m-1">
                         <Card>
-                            <CardImg width="100%" src={props.selectedDish.image} alt={props.selectedDish.name} />
+                            <CardImg width="100%" src={props.dish.image} alt={props.dish.name} />
                             <CardBody>
-                                <CardTitle>{props.selectedDish.name}</CardTitle>
-                                <CardText>{props.selectedDish.description}</CardText>
+                                <CardTitle>{props.dish.name}</CardTitle>
+                                <CardText>{props.dish.description}</CardText>
                             </CardBody>
                         </Card>
                     </div>
